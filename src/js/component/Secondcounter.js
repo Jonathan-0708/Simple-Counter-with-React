20695e1eed39c61bb3ba58ae50d18f54d844bdd2
Jonathan Diaz <64094787+Jonathan-0708@users.noreply.github.com>
2021-07-10 21:26:57 -0400

    cambios finales

--- src/js/component/Secondcounter.js
+++ src/js/component/Secondcounter.js
@@ -6,14 +6,15 @@ const Secondcounter = props => {
 
 	return (
 		<>
-			<div className="bg-dark text-center container-fluid text-white bg-dark  mb-3">
-				<div className="row">
-					<div className="col">
-						<div className="far fa-clock p-2 ">
-							{" "}
-							{`  ${cero1} - ${cero2} - ${cero3} - ${cero4} - ${cero5} - ${cero6}`}
-						</div>
-					</div>
+			<div className="container-fluid fondo mb-3 text-center text-white ">
+				<div className="row justify-content-center">
+					<div className="col-1 pt-4 far fa-clock fondo1 m-2 "></div>
+					<div className="p-2 fondo1 m-2 ">{`${cero1}`} </div>
+					<div className="p-2 fondo1 m-2 ">{`${cero2}`} </div>
+					<div className="p-2 fondo1 m-2 ">{`${cero3}`} </div>
+					<div className="p-2 fondo1 m-2 ">{`${cero4}`} </div>
+					<div className="p-2 fondo1 m-2 ">{`${cero5}`}</div>
+					<div className="p-2 fondo1 m-2 ">{`${cero6}`} </div>
 				</div>
 			</div>
 		</>
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
+import PropTypes from "prop-types";
 //include bootstrap npm library into the bundle
 import "bootstrap";
 
@@ -9,7 +9,22 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
 
+import Secondcounter from "./component/Secondcounter";
+
+let contador = 0;
+let segundos = "";
+
+function timer() {
+	contador += 1;
+	segundos = "000000" + contador;
+	segundos = segundos.slice(-6);
+	ReactDOM.render(
+		<Secondcounter tiempo={segundos.split("")} />,
+		document.querySelector("#app")
+	);
+}
+
+Secondcounter.propTypes = { tiempo: PropTypes.array };
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+window.setInterval(timer, 1000);
