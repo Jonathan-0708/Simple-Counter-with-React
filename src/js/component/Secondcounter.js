75144d3f8654f5227a828cca313edad7b080f904
Jonathan Diaz <64094787+Jonathan-0708@users.noreply.github.com>
2021-07-11 10:20:18 -0400

    otros cambios

--- src/js/component/Secondcounter.js
+++ src/js/component/Secondcounter.js
@@ -1,20 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "bootstrap";
-const Secondcounter = props => {
-	let [cero1, cero2, cero3, cero4, cero5, cero6] = props.tiempo;
 
+const Secondcounter = ({ tiempo }) => {
 	return (
 		<>
 			<div className="container-fluid fondo mb-3 text-center text-white ">
 				<div className="row justify-content-center">
 					<div className="col-1 pt-4 far fa-clock fondo1 m-2 "></div>
-					<div className="p-2 fondo1 m-2 ">{`${cero1}`} </div>
-					<div className="p-2 fondo1 m-2 ">{`${cero2}`} </div>
-					<div className="p-2 fondo1 m-2 ">{`${cero3}`} </div>
-					<div className="p-2 fondo1 m-2 ">{`${cero4}`} </div>
-					<div className="p-2 fondo1 m-2 ">{`${cero5}`}</div>
-					<div className="p-2 fondo1 m-2 ">{`${cero6}`} </div>
+					{tiempo.map((element, index) => {
+						return (
+							<div
+								key={`counterElement-${index}`}
+								className="p-2 fondo1 m-2 ">
+								{`${element}`}
+							</div>
+						);
+					})}
 				</div>
 			</div>
 		</>
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
@@ -19,6 +20,8 @@ function timer() {
 	contador += 1;
 	segundos = "000000" + contador;
 	segundos = segundos.slice(-6);
+	console.log(segundos);
+	console.log(segundos.split(""));
 	ReactDOM.render(
 		<Secondcounter tiempo={segundos.split("")} />,
 		document.querySelector("#app")
